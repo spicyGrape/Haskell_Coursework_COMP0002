139f587395689a01cb8c675bccdbe93977a1cf85
Shuaiting Li <li.private0@gmail.com>
2024-12-22 17:09:27 +0000

    Modified transpose to use higer order funcitons

--- cwk2_24-25.hs
+++ cwk2_24-25.hs
@@ -12,17 +12,10 @@ numberHorse = ["123",
                "789"]
 
 transpose :: Horse -> Horse
-transpose= transposeHorse (repeat [])
+transpose = foldl transposeHorse (repeat []) 
 
-transposeH :: Horse -> Horse
-transposeH = foldl transposeHorseH (repeat []) 
-
-transposeHorse :: Horse -> Horse -> Horse
-transposeHorse h [] = h
-transposeHorse transposed (sourse:sourses) = transposeHorse (zipWith (:) sourse transposed) sourses
-
-transposeHorseH :: Horse -> String -> Horse
-transposeHorseH transposed sourse = zipWith (:) sourse transposed
+transposeHorse :: Horse -> String -> Horse
+transposeHorse transposed sourse = zipWith (:) sourse transposed
 
 pretty :: Horse -> IO()
 pretty [h]    = do
